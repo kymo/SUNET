ea9f5083c1450bb0b5b8df30e99a29d67d5c202c
wangxiaofeng04 <wangxiaofeng04@baidu.com>
2016-10-16 16:15:21 +0800

    add test file

--- client/client.cpp
+++ client/client.cpp
@@ -10,6 +10,10 @@
 #include <fcntl.h>
 #include <sys/shm.h>
 
+#include "thread.h"
+
+using namespace sub_framework;
+
 class Client {
 
 private:
@@ -48,13 +52,40 @@ void Client::_connect_svr() {
 	
 	recv(_clt_sock_fd, buf, 1024, 0);
 	std::cout << buf << std::endl;
+	if (strcmp(buf, "Hello from svr!") == 0) {
+		std::cout << "WRITE!" << std::endl;
+	} else {
+		std::cout << "WRONG!" << std::endl;
+	}
 }
 
-int main() {
 
-	Client* client = new Client();
-	client->_init_svr_param("127.0.0.1", 8888);
-	client->_connect_svr();
+class ClientHandler : public SubThreadHandler {
+public:
+	int _thread_proc_handler(void *args) {
+		Client* client = new Client();
+		client->_init_svr_param("127.0.0.1", 8888);
+		client->_connect_svr();
+	}
+};
 
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		std::cout << "Usag ./client [thread counts]" << std::endl;
+		exit(1);
+	}
+	int thread_num = atoi(argv[1]);
+	std::cout << argv[1] << std::endl;
+	ClientHandler*client_handler = new ClientHandler();
+	std::vector<SubThread*> threads;
+	for (int i = 0; i < thread_num; i++) {
+		std::cout << i << std::endl;
+		SubThread* sub_thread = new SubThread(client_handler, NULL);
+		sub_thread->_start();
+		threads.push_back(sub_thread);
+	}
+	for (int i = 0; i < thread_num; i++) {
+		threads[i]->_join();
+	}
 	return 0;
 }
